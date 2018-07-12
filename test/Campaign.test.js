a6a94173c68ead75554efb27318a181f77cb7ab7
ThalKod <tmarcelin@thal.tech>
2018-07-12 13:33:34 -0400

    add Campaign test suits

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -17,4 +17,103 @@ beforeEach(async ()=>{
     factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
         .deploy({ data: compiledFactory.bytecode })
         .send({ from: accounts[0], gas: "1000000" });
+
+    await factory.methods.createCampaign("100").send({
+        from: accounts[0],
+        gas: "1000000"
+    });
+
+    [campaignAddress] = await factory.methods.getDeployedCampaign().call();
+
+    campaign = await new web3.eth.Contract(
+        JSON.parse(compiledCampaign.interface),
+        campaignAddress
+    );
+}); 
+
+
+describe("Campaigns", ()=>{
+    it("should deploys a factory and a campaign", ()=>{
+        assert.ok(factory.options.address);
+        assert.ok(campaign.options.address);
+    });
+
+    it("should mark caller as the campaign manager", async ()=>{
+        const manager = await campaign.methods.manager().call();
+        assert.equal(accounts[0], manager);
+    });
+
+    it("should allows people to contribute money and marks them as approvers", async ()=>{
+        await campaign.methods.contribute().send({
+            value: "200",
+            from: accounts[1]
+        });
+
+        const isContributor = await campaign.methods.approvers(accounts[1]).call();
+
+        assert(isContributor);
+    });
+
+    it("should requires a minimum contribution", async ()=>{
+
+        try {
+            await campaign.methods.contribute().send({
+                value: "3",
+                from: accounts[1]
+            });
+            assert(false);
+        } catch (err) {
+            assert(err);
+        }
+
+    });
+
+    it("should allows a manager to make a payment request", async ()=>{
+        await campaign.methods.createRequest(
+            "Buy a cars",
+            "100",
+            accounts[1],
+        )
+        .send({
+            from: accounts[0],
+            gas: "1000000"
+        });
+
+        const request = await campaign.methods.requests(0).call();
+
+        assert.equal("Buy a cars", request.description);
+    });
+    
+    it("should processes requests", async ()=>{
+        await campaign.methods.contribute().send({
+            from: accounts[0],
+            value: web3.utils.toWei("10", "ether")
+        });
+
+        await campaign.methods.createRequest(
+            "B",
+            web3.utils.toWei("5", "ether"),
+            accounts[1],
+        )
+        .send({
+            from: accounts[0],
+            gas: "1000000"
+        });
+
+        await campaign.methods.approveRequest(0).send({
+            from: accounts[0],
+            gas: "1000000"
+        });
+
+        await campaign.methods.finalizeRequest(0).send({
+            from: accounts[0],
+            gas: "1000000"
+        });
+
+        let balance = await web3.eth.getBalance(accounts[1]);
+        balance = web3.utils.fromWei(balance, "ether");
+        balance = parseFloat(balance);
+
+        assert(balance > 104);
+    });
 });
