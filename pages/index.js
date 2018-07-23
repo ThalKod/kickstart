541682fef01d03a5c7f079b76de34afdde48ab82
ThalKod <tmarcelin@thal.tech>
2018-07-23 10:28:16 -0400

    rendering deployed campaigns

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,34 @@
 import React from "react";
 import factory from "../ethereum/factory";
+import { Card } from "../node_modules/semantic-ui-react";
 
 
 class CampaignIndex extends React.Component {
 
-    async componentDidMount(){
+    // Fetching props from server...
+    static async getInitialProps(){
         const campaigns = await factory.methods.getDeployedCampaign().call();
 
-        console.log(campaigns);
+        return { campaigns };
+    }
+
+    renderCampaign(){
+        const items = this.props.campaigns.map((address)=>{
+            return {
+                header: address,
+                description: <a>View Details</a>,
+                fluid: true
+            }
+        });
+
+        return <Card.Group items={items} />
     }
 
     render(){
-        return <div>Campaigns Index</div>
+        return <div>
+            <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.1/semantic.min.css"></link>
+            {this.renderCampaign()}
+        </div>
     }
 
 }
