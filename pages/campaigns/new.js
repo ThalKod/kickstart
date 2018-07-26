b545aabc464e66a762cfab1003a7b8bad7e5dcb8
ThalKod <tmarcelin@thal.tech>
2018-07-26 13:53:04 -0400

    handle input and form submit event

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,17 +1,51 @@
 import React from "react";
 import Layout from "../../components/Layout";
-import { Form, Button } from "semantic-ui-react";
+import { Form, Button, Input } from "semantic-ui-react";
+import factory from "../../ethereum/factory";
+import web3 from "../../ethereum/web3";
 
 class CampaignNew extends React.Component {
+    state = {
+        minimumContribution: ""
+    };
+
+    onContributionChange = (e)=>{
+        const contribution = e.target.value;
+
+        this.setState(()=>{
+            return {
+                minimumContribution: contribution
+            }
+        });
+    }
+
+    onFormSubmit = async (e)=>{
+        e.preventDefault();
+
+        const accounts = await web3.eth.getAccounts();    
+        await factory.methods
+                .createCampaign(this.state.minimumContribution)
+                .send({
+                    from: accounts[0]
+                });
+
+    }
+
+
     render(){
         return (
             <Layout>
                 <h3>Create a Campaign</h3>
 
-                <Form>
+                <Form onSubmit={this.onFormSubmit}>
                     <Form.Field>
                         <label>Minimum Contribution</label>
-                        <input type="text"/>
+                        <Input  
+                            label="wei" 
+                            labelPosition="right"
+                            value={this.state.minimumContribution}
+                            onChange={this.onContributionChange}
+                        />
                     </Form.Field>
 
                     <Button primary>Create !</Button>
