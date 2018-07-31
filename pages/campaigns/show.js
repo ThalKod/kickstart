654d5e26df4f73fe7fed54953bcb2463c0f01b9e
ThalKod <tmarcelin@thal.tech>
2018-07-31 14:25:14 -0400

    add single campaigns view routes

---
+++ pages/campaigns/show.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class CampaignShow extends React.Component {
+    render(){
+        return (
+            <h3>Campaign Show</h3>
+        )
+    }
+}
+
+export default CampaignShow;
