39bc755dbf93dea1c19f9aaf149ecf4de629f009
ThalKod <tmarcelin@thal.tech>
2018-08-03 14:34:53 -0400

    add coresponding card group

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Layout from "../../components/Layout";
 import Campaign from "../../ethereum/campaign";
+import { Card } from "../../node_modules/semantic-ui-react";
+import web3 from "../../ethereum/web3";
 
 class CampaignShow extends React.Component {
 
@@ -9,14 +11,57 @@ class CampaignShow extends React.Component {
         const campaign = Campaign(props.query.address);
 
         const summary = await campaign.methods.getSummarry().call();
-        console.log(summary);
-        return {};
+        
+        return { 
+            minimumContribution: summary["0"],
+            balance: summary["1"],
+            requestCount: summary["2"],
+            approversCount: summary["3"],
+            manager: summary["4"]
+         };
+    }
+
+    renderCards(){
+
+        const { balance, manager, minimumContribution, approversCount, requestCount } = this.props;
+
+        const items = [
+            {
+                header: manager,
+                meta: "Address of Manager",
+                description: "The Manager created this campaign and can create requests to withdraw money",
+                style: { overflowWrap: "break-word" }
+            },
+            {
+                header: minimumContribution,
+                meta: "Minimum Contribution(wei)",
+                description: "You must contribute at least this much wei to be a approver "
+            },
+            {
+                header: requestCount,
+                meta: "Number of Requests",
+                description: " A requests tries to withdraw money from the contract. Request must be approved by approver."
+            },
+            {
+                header: approversCount,
+                meta: "Number of Approvers",
+                description: "Number of people who already donate to the campaign "
+            },
+            {
+                header: web3.utils.fromWei(balance, "ether"),
+                meta: "Campaign Balance (ETH)",
+                description: "The Balance is how much money this campaign has left to spend "
+            }
+        ];
+
+        return <Card.Group items={items} />
     }
 
     render(){
         return (
             <Layout>
                 <h3>Campaign Show</h3>
+                {this.renderCards()}
             </Layout>
         )
     }
