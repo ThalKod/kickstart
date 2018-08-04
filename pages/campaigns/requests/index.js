1191bc5a57d8713b2dffa1d604d352d59d03a2a4
ThalKod <tmarcelin@thal.tech>
2018-08-04 13:29:44 -0400

    add new create request routes

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,10 +1,27 @@
 import React from "react";
+import Layout from "../../../components/Layout";
+import { Link }  from "../../../routes";
+import { Button } from "semantic-ui-react";
 
 class RequestIndex extends React.Component{
+
+    static async getInitalProps(props){
+        const { address } = props.query;
+
+        return { address };
+    }
+
     render(){
         return(
-            <h3>Request List</h3>
-        )
+            <Layout>
+                <h3>Request List</h3>
+                <Link route={`/campaigns/${this.props.address}/requests/new`}>
+                    <a>
+                        <Button primary>Add Requests</Button>
+                    </a>
+                </Link>
+            </Layout>
+        );
     }
 };
 
---
+++ pages/campaigns/requests/new.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class RequestNew extends React.Component {
+    render(){
+        return(
+            <h3>New Request</h3>
+        );
+    }
+};
+
+export default RequestNew;
