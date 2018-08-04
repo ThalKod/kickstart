01e7e917942bced255370703225623e1a1b3d034
ThalKod <tmarcelin@thal.tech>
2018-08-04 13:00:34 -0400

    handling error and loading button on contributeForm

--- components/ContributeForm.js
+++ components/ContributeForm.js
@@ -7,7 +7,10 @@ import { Router } from "../routes";
 class ContributeForm extends React.Component{
 
     state = {
-        value: ""
+        value: "",
+        error: "",
+        loading: false,
+        success: false
     };
 
     onChange = (e)=>{
@@ -16,8 +19,10 @@ class ContributeForm extends React.Component{
 
     onSubmit = async (e)=>{
         e.preventDefault();
-        
+
         const campaign = Campaign(this.props.address);
+
+        this.setState({ loading: true, error: "", success: false });
 
         try{
             const accounts = await web3.eth.getAccounts();
@@ -25,21 +30,27 @@ class ContributeForm extends React.Component{
                 from: accounts[0],
                 value: web3.utils.toWei(this.state.value, "ether")
             });
-            
-            Router.replaceRoute(`/campaigns/${this.props.address}`);
-        }catch(e){
 
+            Router.replaceRoute(`/campaigns/${this.props.address}`);
+
+            this.setState({ success: true });
+        }catch(e){
+            this.setState({ error: e.message });
         }
+
+        this.setState({ loading: false, value: ""});
     }
 
     render(){
         return (
-            <Form onSubmit={this.onSubmit}>
+            <Form onSubmit={this.onSubmit} error={!!this.state.error}>
                 <Form.Field>
                     <label>Amount to Contribute</label>
                     <Input value={this.state.value} onChange={this.onChange} label="ether" labelPosition="right"/>
                 </Form.Field>
-                <Button primary> Contribute </Button>
+                <Button loading={this.state.loading} primary> Contribute </Button>
+                <Message error header="Error" content={this.state.error} />
+                {this.state.success && <Message positive  header="Success" content={`Succefully contributed to ${this.props.address} `} />}
             </Form>
         )
     }
