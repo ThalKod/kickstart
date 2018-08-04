95b85237c11e34ab18bb0c63e960771a55684b58
ThalKod <tmarcelin@thal.tech>
2018-08-04 13:15:38 -0400

    add request Button and Link

---
+++ pages/campaigns/requests/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class RequestIndex extends React.Component{
+    render(){
+        return(
+            <h3>Request List</h3>
+        )
+    }
+};
+
+export default RequestIndex;
