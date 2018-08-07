86a6c1293c9f5d2e464d0b9584ba72b9e0ffab09
ThalKod <tmarcelin@thal.tech>
2018-08-07 15:08:13 -0400

    fetching list of request...

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -2,14 +2,25 @@ import React from "react";
 import Layout from "../../../components/Layout";
 import { Link }  from "../../../routes";
 import { Button } from "semantic-ui-react";
+import Campaign from "../../../ethereum/campaign";
 
 class RequestIndex extends React.Component{
 
     static async getInitialProps(props){
+        console.log("hey");
+
         const { address } = props.query;
 
+        const campaign = Campaign(address);
+        const requestsCount = await campaign.methods.getRequestCount().call();
 
-        return { address };
+        const requests = await Promise.all(
+            Array(requestsCount).fill().map((Element, index)=>{
+                return campaign.methods.requests(index).call();
+            })
+        );
+
+        return { address, requests, requestsCount };
     }
 
     render(){
